dacc627e5c5a4c7e3dbfdab4f416d929a48755ac
Lauren Long <laurenzlong@users.noreply.github.com>
2018-04-03 11:05:24 -0700

    Remove chai-as-promised (#7)

--- spec/index.spec.ts
+++ spec/index.spec.ts
@@ -20,10 +20,6 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
-import * as chai from 'chai';
-import * as chaiAsPromised from 'chai-as-promised';
-chai.use(chaiAsPromised);
-
 import 'mocha';
 import './lifecycle.spec';
 import './main.spec';
