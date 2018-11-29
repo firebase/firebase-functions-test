2a177c7d5fcade90fa047a945387159fc99f3f96
Abe Haskins <abeisgreat@abeisgreat.com>
2018-11-29 19:17:50 +0000

    Adds test for https auth params

--- spec/providers/https.spec.ts
+++ spec/providers/https.spec.ts
@@ -7,25 +7,39 @@ const cfToUpperCaseOnRequest = functions.https.onRequest((req, res) => {
 });
 
 const cfToUpperCaseOnCall = functions.https.onCall((data, context) => {
-    return {msg: data.message.toUpperCase()};
+    const result = {
+        msg: data.message.toUpperCase(),
+        from: 'anonymous',
+    };
+
+    if (context.auth && context.auth.uid) {
+        result.from = context.auth.uid;
+    }
+
+    return result;
 });
 
 describe('providers/https', () => {
-    describe('#wrap', () => {
-        it('should not throw when passed onRequest function', async () => {
-            const test = fft();
-            /*
-                Note that we must cast the function to any here because onRequst functions
-                do not fulfill Runnable<>, so these checks are solely for usage of this lib
-                in JavaScript test suites.
-             */
-            expect(() => test.wrap(cfToUpperCaseOnRequest as any)).to.throw();
-        });
+    it('should not throw when passed onRequest function', async () => {
+        const test = fft();
+        /*
+            Note that we must cast the function to any here because onRequst functions
+            do not fulfill Runnable<>, so these checks are solely for usage of this lib
+            in JavaScript test suites.
+         */
+        expect(() => test.wrap(cfToUpperCaseOnRequest as any)).to.throw();
+    });
 
-        it('should run the wrapped onCall function and return result', async () => {
-            const test = fft();
-            const result = await test.wrap(cfToUpperCaseOnCall)({message: 'lowercase'});
-            expect(result).to.deep.equal({msg: 'LOWERCASE'});
-        });
+    it('should run the wrapped onCall function and return result', async () => {
+        const test = fft();
+        const result = await test.wrap(cfToUpperCaseOnCall)({message: 'lowercase'});
+        expect(result).to.deep.equal({msg: 'LOWERCASE', from: 'anonymous'});
+    });
+
+    it('should accept auth params', async () => {
+        const test = fft();
+        const options = {auth: {uid: 'abc'}};
+        const result = await test.wrap(cfToUpperCaseOnCall)({message: 'lowercase'}, options);
+        expect(result).to.deep.equal({msg: 'LOWERCASE', from: 'abc'});
     });
 });
