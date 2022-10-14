dc29fefd42e96fbe803c8f474fd76a463c572e80
Daniel Young Lee <taeold@gmail.com>
2022-10-14 10:17:46 -0700

    Fix lint issues.

--- src/cloudevent/mocks/helpers.ts
+++ src/cloudevent/mocks/helpers.ts
@@ -1,5 +1,5 @@
 import * as v1 from 'firebase-functions';
-import * as v2  from 'firebase-functions/v2';
+import * as v2 from 'firebase-functions/v2';
 import { Expression } from 'firebase-functions/params';
 
 export const APP_ID = '__APP_ID__';
