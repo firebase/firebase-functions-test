0101a32f9d4242658043f263ad37f35f1241cf17
a.cerutti <a.cerutti@audioboost.it>
2025-10-30 21:33:23 +0100

    Added implementation of Scheduled functions inside V2

--- src/v2.ts
+++ src/v2.ts
@@ -25,7 +25,15 @@ import { CallableFunction, CallableRequest } from 'firebase-functions/v2/https';
 
 import { generateCombinedCloudEvent } from './cloudevent/generate';
 import { DeepPartial } from './cloudevent/types';
-import * as express from 'express';
+import {
+  ScheduledEvent,
+  ScheduleFunction,
+} from 'firebase-functions/v2/scheduler';
+
+type V2WrappableFunctions =
+  | CloudFunction<any>
+  | CallableFunction<any, any>
+  | ScheduleFunction;
 
 /** A function that can be called with test data and optional override values for {@link CloudEvent}
  * It will subsequently invoke the cloud function it wraps with the provided {@link CloudEvent}
@@ -38,19 +46,37 @@ export type WrappedV2CallableFunction<T> = (
   data: CallableRequest
 ) => T | Promise<T>;
 
-function isCallableV2Function<T extends CloudEvent<unknown>>(
-  cf: CloudFunction<T> | CallableFunction<any, any>
+export type WrappedV2ScheduledFunction = (
+  data: ScheduledEvent
+) => void | Promise<void>;
+
+function isCallableV2Function(
+  cf: V2WrappableFunctions
 ): cf is CallableFunction<any, any> {
-  return !!cf?.__endpoint?.callableTrigger;
+  return !!cf.__endpoint?.callableTrigger;
+}
+
+function isScheduledV2Function(
+  cf: V2WrappableFunctions
+): cf is ScheduleFunction {
+  return !!cf.__endpoint?.scheduleTrigger;
 }
 
 function assertIsCloudFunction<T extends CloudEvent<unknown>>(
-  cf: CloudFunction<T> | CallableFunction<any, any>
+  cf: V2WrappableFunctions
 ): asserts cf is CloudFunction<T> {
   if (!('run' in cf) || !cf.run) {
     throw new Error(
       'This library can only be used with functions written with firebase-functions v3.20.0 and above'
     );
+  }
+}
+
+function assertIsCloudFunctionV2<T extends CloudEvent<unknown>>(
+  cf: CloudFunction<T> | CallableFunction<any, any>
+): asserts cf is CloudFunction<T> {
+  if (cf?.__endpoint?.platform !== 'gcfv2') {
+    throw new Error('This function can only wrap V2 CloudFunctions.');
   }
 }
 
@@ -70,19 +96,34 @@ export function wrapV2(
   cloudFunction: CallableFunction<any, any>
 ): WrappedV2CallableFunction<any>;
 
+export function wrapV2(
+  cloudFunction: ScheduleFunction
+): WrappedV2ScheduledFunction;
+
 export function wrapV2<T extends CloudEvent<unknown>>(
-  cloudFunction: CloudFunction<T> | CallableFunction<any, any>
-): WrappedV2Function<T> | WrappedV2CallableFunction<any> {
+  cloudFunction:
+    | CloudFunction<T>
+    | CallableFunction<any, any>
+    | ScheduleFunction
+):
+  | WrappedV2Function<T>
+  | WrappedV2CallableFunction<any>
+  | WrappedV2ScheduledFunction {
+  if (!cloudFunction) {
+    throw new Error('Cannot wrap: undefined cloud function');
+  }
+
+  assertIsCloudFunction(cloudFunction);
+  assertIsCloudFunctionV2(cloudFunction);
+
   if (isCallableV2Function(cloudFunction)) {
     return (req: CallableRequest) => {
       return cloudFunction.run(req);
     };
   }
 
-  assertIsCloudFunction(cloudFunction);
-
-  if (cloudFunction?.__endpoint?.platform !== 'gcfv2') {
-    throw new Error('This function can only wrap V2 CloudFunctions.');
+  if (isScheduledV2Function(cloudFunction)) {
+    return createScheduledWrapper(cloudFunction);
   }
 
   return (cloudEventPartial?: DeepPartial<T>) => {
@@ -92,4 +133,26 @@ export function wrapV2<T extends CloudEvent<unknown>>(
     );
     return cloudFunction.run(cloudEvent);
   };
+}
+
+function createScheduledWrapper(
+  cloudFunction: ScheduleFunction
+): WrappedV2ScheduledFunction {
+  return function(options: ScheduledEvent) {
+    _checkOptionValidity(['jobName', 'scheduleTime'], options);
+    return cloudFunction.run(options);
+  };
+}
+
+function _checkOptionValidity(
+  validFields: string[],
+  options: Record<string, any>
+) {
+  Object.keys(options).forEach((key) => {
+    if (validFields.indexOf(key) === -1) {
+      throw new Error(
+        `Options object ${JSON.stringify(options)} has invalid key "${key}"`
+      );
+    }
+  });
 }
